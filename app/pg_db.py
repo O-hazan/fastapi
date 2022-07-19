4a7613f277de87fe2bed3647a5444a657d5a72ec
Omer Hazan <omerathome@gmail.com>
2022-07-19 08:43:11 +0200

    make it better

--- app/pg_db.py
+++ app/pg_db.py
@@ -4,13 +4,15 @@ import psycopg2
 
 
 ## Postgres Database
-DATABASE_URL ="postgres://blxsdwppssurjd:a66c484545a15835d10746ad930d8686d304e7a06defff5a5dab0306b230d869@ec2-54-228-32-29.eu-west-1.compute.amazonaws.com:5432/d28hmilckokoaq"
-# DATABASE_URL = 'postgres://smjzhatreukulg:f0814424648525098f9c827986b6d1c8fe00b1cb3edf2e6cc90b04f1bdf42d3c@ec2-3-219-52-220.compute-1.amazonaws.com:5432/d2ss7j4k7frufr'
+# DATABASE_URL ="postgres://blxsdwppssurjd:a66c484545a15835d10746ad930d8686d304e7a06defff5a5dab0306b230d869@ec2-54-228-32-29.eu-west-1.compute.amazonaws.com:5432/d28hmilckokoaq"
+DATABASE_URL = 'postgres://smjzhatreukulg:f0814424648525098f9c827986b6d1c8fe00b1cb3edf2e6cc90b04f1bdf42d3c@ec2-3-219-52-220.compute-1.amazonaws.com:5432/d2ss7j4k7frufr'
 # OLD LIVE DATABASE_URL = 'postgresql://nlipxarhtxeupm:78a61e9596b97351abaad96528a80bc228e7413d28aef8c7dccc377f0997879c@ec2-34-242-84-130.eu-west-1.compute.amazonaws.com:5432/de2nkfj8f5c9r8'
-# DATABASE_URL = os.environ["DATABASE_URL"]
 # added when trying to connect to Heroku
-# conn = psycopg2.connect(DATABASE_URL, sslmode='require') 
 # DATABASE_URL = "postgresql://myuser:123456@localhost:5432/postgres_database"
+# DATABASE_URL = os.environ["DATABASE_URL"]
+# conn = psycopg2.connect(DATABASE_URL, sslmode='require') 
+
+
 # postgresql://usertest:usertest222@127.0.0.1:5432/dbtest
 
 database = databases.Database(DATABASE_URL)
