f56a484f78e83f82c44364e247361888d29f4e08
Omer Hazan <omerathome@gmail.com>
2022-07-24 14:10:24 +0200

    connect to stg db

--- app/pg_db.py
+++ app/pg_db.py
@@ -5,7 +5,9 @@ import psycopg2
 
 ## Postgres Database
 
-DATABASE_URL = 'postgresql://smjzhatreukulg:f0814424648525098f9c827986b6d1c8fe00b1cb3edf2e6cc90b04f1bdf42d3c@ec2-3-219-52-220.compute-1.amazonaws.com:5432/d2ss7j4k7frufr'
+# DATABASE_URL = 'postgresql://smjzhatreukulg:f0814424648525098f9c827986b6d1c8fe00b1cb3edf2e6cc90b04f1bdf42d3c@ec2-3-219-52-220.compute-1.amazonaws.com:5432/d2ss7j4k7frufr'
+
+DATABASE_URL = 'postgresql://blxsdwppssurjd:a66c484545a15835d10746ad930d8686d304e7a06defff5a5dab0306b230d869@ec2-54-228-32-29.eu-west-1.compute.amazonaws.com:5432/d28hmilckokoaq'
 # DATABASE_URL = "postgresql://myuser:123456@localhost:5432/postgres_database" 
 # DATABASE_URL = os.environ["DATABASE_URL"]  
 # added when trying to connect to Heroku - related to connection from local to postgres docker
